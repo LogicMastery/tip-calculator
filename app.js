93b860aa5dec5f3ffdb9fff8badfa8e3c1561b03
Jean Dale Joseph <silicon.logic.tech@gmail.com>
2024-04-15 17:56:43 +0800

    updated my js file by adding the code comments, about how my functions work?

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-/* We first created a function */
+/* We first created a function and we name it Calculate(), then we created a new variable called billAmount is getting the id from HTML called billAmount
+then we made it Number() and added .value at the end, then after that we created a new variable in a next line called tipAmount in this variable
+we're getting the ID tipAmount from the HTML document, then we called the tipAmount and we set it to Number(tipAmount.value) then we set the 
+tipAmount variable to tipAmount / 100; 9 -> 0.09.
+
+Next we created totalTip variable and set it billAmount * tipAmount it multiplies the value of billAmount and the tipAmount.
+Then we created a variable called tipHTML we set the tipHTML to "<h3> Total tip: <small>$" then concatenate it to totalTip.toFixed(2) + "</small></h3>"
+toFixed means we add a decimal points at the end of the value plus 2 zeros.
+Next, we then create a document.querySelector(".tip-amount").innerHTML = tipHTML - we are calling the class called tip-amount and then we set it to inner.HTML
+and we call the tipHTML variable to show the total tip in my web page
+
+*/
 function Calculate(){
     var billAmount = Number(document.getElementById("billAmount").value)
     var tipAmount = document.getElementById("tipAmount")
